3991475948f69f65b4b7ab84c6a3aff39a817f57
PhuHoang07 <duongphu1207@gmail.com>
2023-10-15 15:03:46 +0700

    reup file

---
+++ ESMS Data/Models/ExamTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ESMS_Data.Models
+{
+    public partial class ExamTime
+    {
+        public ExamTime()
+        {
+            ExamSchedules = new HashSet<ExamSchedule>();
+            Registrations = new HashSet<Registration>();
+        }
+
+        public int Idt { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Start { get; set; }
+        public TimeSpan End { get; set; }
+        public int SlotId { get; set; }
+
+        public virtual Slot Slot { get; set; }
+        public virtual ICollection<ExamSchedule> ExamSchedules { get; set; }
+        public virtual ICollection<Registration> Registrations { get; set; }
+    }
+}
